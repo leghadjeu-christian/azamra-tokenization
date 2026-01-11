a264de284d55a14c4ad01795c42fd4013750dc5a
Marc-Jr <atebemarcjr@icloud.com>
2026-01-11 09:54:33 +0100

    fix: address PR review comments

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-"use client";
-
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useState } from "react";
 import { ThemeProvider } from "@/components/theme-provider";
